f32ea2772e65d19c02b2d09a433652b6b4a0abff
Avyyukt Ajith <avyyukt.ajith@gmail.com>
2023-06-19 18:55:11 +0530

    remove old server & client

--- client.py
+++ client.py
@@ -4,7 +4,10 @@ import os
 import socket
 import sys
 import zlib
-from typing import Optional
+import json
+import base64
+import argparse
+from typing import Optional, Dict
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 6969  # The port used by the server
@@ -14,7 +17,7 @@ class ClientProtocol:
 
     def __init__(self, socket):
         self.socket = socket
-        self.encoding = "ascii"
+        self.encoding = "utf-8"
     
     def readline(self) -> Optional[str]:
         line = ""
@@ -41,91 +44,145 @@ class ClientProtocol:
     def writeline(self, line: str):
         self.socket.sendall(bytes(line+"\n", self.encoding))
     
-    def message_type(self, type: str):
-        self.writeline(f"message-type: {type}")
+    def send(self, data: Dict):
+        self.writeline(json.dumps(data))
     
-    def pair(self, key: str, value: str):
-        self.writeline(f"{key}: {value}")
-    
-    def read_pair(self) -> Optional[str]:
+    def response(self) -> Optional[Dict]:
         line = self.readline()
-        if line is None:
+        if not line:
             return None
-        kv_pair = line.split(":")
-        key, value = map(str.strip, kv_pair)
-        return key, value
-    
-    def read_key(self, key: str) -> Optional[str]:
-        pair = self.read_pair()
-        if pair is None:
-            return None
-        k, v = pair
-        assert k == key, f"invalid key: {k}, expected: {key}"
-        return v
-    
+        return json.loads(line)
     
     def register(self, username: str, password: str) -> str:
-        self.message_type("register")
-        self.pair("username", username)
-        self.pair("password", password)
-        data = self.read_pair()
-        assert data[0] == "result"
-        return data[1]
+        self.send({
+            "type": "register",
+            "username": username,
+            "password": password
+        })
+
+        return self.response()
     
     def upload(self, path: str):
-        self.message_type("upload")
-        self.pair("name", os.path.basename(path))
-        with open(path, "rb") as f:
-            data = f.read()
-        
-        # compress at maximum level
-        # trades cpu time for smaller size
-        data = zlib.compress(data, level=9)
+        token = self.token()
 
-        self.pair("bytes", len(data))
-        # Send the raw file over the socket
-        self.socket.sendall(data)
-        data = self.read_pair()
-        assert data[0] == "result"
-        return data[1]
+        if token is None:
+            return
+        with open(path, "rb") as f:
+            content = f.read()
+        
+        content = zlib.compress(content, level=9)
+        b64 = base64.b64encode(content).decode(self.encoding)
+
+        self.send({
+            "type": "upload",
+            "name": os.path.basename(path),
+            "content": b64,
+            "token": token
+        })
+
+        return self.response()
     
-    def download(self, name: str):
-        self.message_type("download")
-        self.pair("name", name)
-        nbytes = self.read_key("bytes")
-        content = self.read_bytes(int(nbytes))
+    def download(self, name: str, output: Optional[str] = None):
+        token = self.token()
+
+        if token is None:
+            return
+        
+        self.send({
+            "type": "download",
+            "name": name,
+            "token": token
+        })
+
+        response = self.response()
+        assert response["result"] == "success"
+        content = base64.b64decode(response["content"])
         content = zlib.decompress(content)
-        with open(name, "wb") as f:
+
+        if output is None:
+            output = name
+        
+    
+        with open(output, "wb") as f:
             f.write(content)
+    
+    def login(self, username: str, password: str):
+        if os.path.exists("token.json"):
+            print("Already logged in.")
+            return
+        
+        self.send({
+            "type": "login",
+            "username": username,
+            "password": password
+        })
+
+        token = self.response()
+        if token["result"] != "success":
+            print(f"Login failed: {token['message']}")
+        else:
+            print(f"Login successful.")
+            with open("token.json", "w") as f:
+                json.dump(token, f)
+    
+    def token(self) -> Optional[Dict]:
+        if not os.path.exists("token.json"):
+            print("You have not logged in yet.")
+            return None
+        
+        with open("token.json", "r") as f:
+            return json.load(f)
 
     def close(self):
         self.message_type("close")
         self.socket.close()
 
 
-if len(sys.argv) < 2:
-    print("Usage: client.py <command> [args...]")
-    sys.exit(0)
+
+def main():
+    parser = argparse.ArgumentParser(description='Delta Fileserver Client')
+
+    subparsers = parser.add_subparsers(title='subcommands', dest='subcommand')
+
+    # Register subcommand
+    register_parser = subparsers.add_parser('register', help='register a user')
+    register_parser.add_argument('username', type=str, help='Username')
+    register_parser.add_argument('password', type=str, help='Password')
 
 
-
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.connect((HOST, PORT))
-p = ClientProtocol(socket)
-
-cmd = sys.argv[1]
-if cmd == "register":
-    username = sys.argv[2]
-    password = sys.argv[3]
-    result = p.register(username, password)
-    print(result)
-elif cmd == "upload":
-    path = sys.argv[2]
-    result = p.upload(path)
-    print(result)
-elif cmd == "download":
-    name = sys.argv[2]
-    p.download(name)
+    # Upload subcommand
+    upload_parser = subparsers.add_parser('upload', help='upload a file')
+    upload_parser.add_argument('path', type=str, help='path to file to be uploaded')
 
 
-p.message_type("close")
+    # Download subcommand
+    download_parser = subparsers.add_parser('download', help='download a file')
+    download_parser.add_argument('name', type=str, help='file name to download')
+    # Add an optional output file
+    download_parser.add_argument('-o', '--output', type=str, required=False, help='output file to given path')
+
+    login_parser = subparsers.add_parser('login', help='login using credentials')
+    login_parser.add_argument('username', type=str, help='username')
+    login_parser.add_argument('password', type=str, help='password')
+
+
+    args = parser.parse_args()
+    if args.subcommand is None:
+        parser.print_help()
+        sys.exit(1)
+    
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((HOST, PORT))
+    p = ClientProtocol(sock)
+
+    if args.subcommand == "register":
+        print(p.register(args.username, args.password))
+    elif args.subcommand == "upload":
+        print(p.upload(args.path))
+    elif args.subcommand == "download":
+        p.download(args.name, args.output)
+    elif args.subcommand == "login":
+        p.login(args.username, args.password)
+
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -1,131 +1,198 @@
 import atexit
+import base64
 import hmac
 import json
-import base64
 import logging
 import threading
+from functools import wraps
 from socketserver import StreamRequestHandler, ThreadingTCPServer
-from typing import Optional
+from typing import Dict, Optional
 
-logging.basicConfig(level=logging.INFO, format='[%(name)s]: %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='[%(name)s]: %(message)s')
 log = logging.getLogger('server')
 
+ENCODING = 'utf-8'
+# im hardcoding it cuz im lazy
+# but ill load from an environment variable
+# for "production" lmao
+SECRET_KEY = b"LOAD_FROM_ENVVAR_THIS_IS_UNSAFE_LMAOOO_IDGAF"
 
+class DataAccessLayer:
 
-class DataStore:
     def __init__(self):
         self.files = {}
-        self.passwd = {}
-    
-    def user_exists(self, username: str) -> bool:
-        return username in self.passwd
-    
-    def store_file(self, name: str, content: bytes):
-        self.files[name] = content
+        self.users = {}
+        self.owners = {}
 
-DATA = DataStore()
+    def store_file(self, username: str, name: str, content: bytes):
+        if username not in self.owners:
+            self.owners[username] = []
+        self.owners[username].append(name)
+        self.files[name] = content
+    
+    def check_user_exists(self, username: str) -> bool:
+        return username in self.users
+    
+    def authenticate(self, username: str, password: str):
+        return self.users.get(username) == password
+
+DATA = DataAccessLayer()
 
 class ServerProtocol:
 
-    def __init__(self, rfile, wfile):
-        self.rfile = rfile
-        self.wfile = wfile
-        self.encoding = "ascii"
+    def handle(self, data: Dict):
+        handlers = {
+            "register": self.handle_register,
+            "upload": self.authorized_only(self.handle_upload),
+            "download": self.authorized_only(self.handle_download),
+            "login": self.handle_login
+        }
 
-    def read_bytes(self, nbytes: int) -> bytes:
-        return self.rfile.read(nbytes)
-    
-    def write_bytes(self, content: bytes):
-        self.wfile.write(content)
+        ty = data["type"]
 
-    def readline(self) -> Optional[str]:
-        line = self.rfile.readline()
-        if not line:
-            return None
-        ret = line.decode(self.encoding).rstrip()
-        log.debug(ret)
-        return ret
-    
-    def type(self) -> Optional[str]:
-        line = self.readline()
-        if line is None:
-            return None
-        kv_pair = line.split(":")
-        key, value = map(str.strip, kv_pair)
+        handler = handlers.get(ty, lambda _: {
+            "result": "error",
+            "message": f"unknown message type: {ty}"
+        })
 
-        assert key == "message-type", "invalid message type"
-        return value
+        return handler(data)
     
-    def pair(self) -> Optional[str]:
-        line = self.readline()
-        if line is None:
-            return None
-        kv_pair = line.split(":")
-        key, value = map(str.strip, kv_pair)
-        return key, value
-    
-    def send_pair(self, key: str, value: str):
-        self.writeline(f"{key}: {value}")
-    
-    def key(self, key: str) -> Optional[str]:
-        pair = self.pair()
-        if pair is None:
-            return None
-        k, v = pair
-        assert k == key, f"invalid key: {k}, expected: {key}"
-        return v
-    
+    def authorize(self, token: Dict) -> Optional[Dict]:
+        username = token["username"]
+        signature = token["sign"]
+        computed = self.compute_signature(username)
+        return computed == signature
 
-    def writeline(self, line: str):
-        self.wfile.write(bytes(line+"\n", self.encoding))
     
-    def register(self):
-        username = self.key("username")
-        password = self.key("password")
-        if not DATA.user_exists(username):
-            DATA.passwd[username] = password
-            self.send_pair("result", "success")
-        else:
-            self.send_pair("result", "exists")
-    
-    def upload(self):
-        name = self.key("name")
-        nbytes = int(self.key("bytes"))
-        content = self.read_bytes(nbytes)
-        DATA.store_file(name, content)
-        self.send_pair("result", "success")
-    
-    def download(self):
-        """
-        From client:
-        message-type: download
-        name: <name>
+    def handle_register(self, data: Dict):
+        assert data["type"] == "register"
+        username = data["username"]
+        password = data["password"]
 
-        Server:
-        bytes: <bytes>
-        <content>
-        """        
-        name = self.key("name")
+        if DATA.check_user_exists(username):
+            return {
+                "result": "error",
+                "message": f"user already exists: {username}"
+            }
+        
+        DATA.users[username] = password
+
+        return {
+            "result": "success"
+        }
+    
+    def handle_upload(self, data: Dict):
+        assert data["type"] == "upload"
+        token = data["token"]
+
+        if "token" not in data or not self.authorize(token):
+            return {
+                "result": "error",
+                "message": "invalid token"
+            }
+        
+        name = data["name"]
+        username = token["username"]
+        content = data["content"]
+        decoded = base64.b64decode(content)
+        DATA.store_file(username, name, decoded)
+
+        return {
+            "result": "success"
+        }
+    
+    def handle_download(self, data: Dict):
+        assert data["type"] == "download"
+        name = data["name"]
+        if name not in DATA.files:
+            return {
+                "result": "error",
+                "message": f"no such file: {name}"
+            }
         content = DATA.files[name]
-        self.send_pair("bytes", len(content))
-        self.write_bytes(content)
+        encoded = base64.b64encode(content).decode(ENCODING)
+        return {
+            "result": "success",
+            "content": encoded
+        }
+    
+    def handle_login(self, data: Dict):
+        username = data["username"]
+        password = data["password"]
+
+        if not DATA.check_user_exists(username):
+            return {
+                "result": "error",
+                "message": f"no such user: {username}. please register first"
+            }
+        
+        if not DATA.authenticate(username, password):
+            return {
+                "result": "error",
+                "message": f"invalid credentials for user: {username}"
+            }
+        
+        # We generate a token for the user
+        # and send it back to the client
+        return {
+            "result": "success",
+            "username": username,
+            "sign": self.compute_signature(username)
+        }
+    
+    @staticmethod
+    def compute_signature(string: str):
+        signature = hmac.new(SECRET_KEY, string.encode(ENCODING), "sha256").digest()
+        signature = base64.b64encode(signature).decode(ENCODING)
+        return signature
+    
+    def authorized_only(self, func):
+
+        @wraps(func)
+        def _authorized_handler(data: Dict):
+            if "token" not in data:
+                return {
+                    "result": "error",
+                    "message": "missing token (invalid json protocol)"
+                }
+            
+            token = data["token"]
+            if not self.authorize(token):
+                return {
+                    "result": "error",
+                    "message": "invalid token"
+                }
+            
+            return func(data)
+    
+        return _authorized_handler
+
 
 class RequestHandler(StreamRequestHandler):
     timeout = 5
 
     def handle(self):
         log.info(f"connected to {self.client_address}")
-        protocol = ServerProtocol(self.rfile, self.wfile)
+        protocol = ServerProtocol()
         while True:
-            ty = protocol.type()
-            if ty == "register":
-                protocol.register()
-            elif ty == "upload":
-                protocol.upload()
-            elif ty == "download":
-                protocol.download()
-            if ty is None or ty == "close":
+            line = self.rfile.readline().rstrip()
+            if not line:
                 break
+
+            try:
+                data = json.loads(line)
+                log.debug(f"got: {data}")
+            except json.JSONDecodeError:
+                log.error(f"received invalid json: {line}")
+                break
+
+            response = protocol.handle(data)
+            self.wfile.write(
+                bytes(
+                    json.dumps(response)+"\n", 
+                    ENCODING
+                )
+            )
 
 @atexit.register
 def cleanup():
